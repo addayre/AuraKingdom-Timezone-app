d29276494b755d2b081f56efce900969a120c473
Adrian L. Silva <laurindoadrian@gmail.com>
2023-01-11 09:14:50 -0300

    finished styling & new fonts

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      titleBarStyle: 'hidden',
+      titleBarOverlay: {
+        color: '#2f3241',
+        symbolColor: '#74b1be',
+      }
     })
   
     win.loadFile('index.html')
