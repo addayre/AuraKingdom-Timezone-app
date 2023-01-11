355eccb6ef4486453d9e6f4384ededc3e3334c2d
Adrian L. Silva <laurindoadrian@gmail.com>
2023-01-11 07:08:42 -0300

    back-end finished

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+      width: 800,
+      height: 600
     })
-
+  
     win.loadFile('index.html')
-}
+  }
 
-app.whenReady().then(() => {
+  app.whenReady().then(() => {
     createWindow()
-})
+  })
